01aa1ca05e916b81e2574e875f18286ac776ae39
James Xue <jamesxue100@gmail.com>
2015-10-16 15:33:54 -0400

    ported to express, added compass

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
-var http = require('http');
-var path = require('path');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var path = require("path");
 
-http.createServer(function (request, response) {
-    console.log('request starting...');
-	
-	var filePath = '.' + request.url;
-	if (filePath == './')
-		filePath = './googlemap.html';
-	
-	fs.exists(filePath, function(exists) {
-	
-		if (exists) {
-			fs.readFile(filePath, function(error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				}
-				else {
-					response.writeHead(200, { 'Content-Type': 'text/html' });
-					response.end(content, 'utf-8');
-				}
-			});
-		}
-		else {
-			response.writeHead(404);
-			response.end();
-		}
-	});
-	
-}).listen(3000);
-console.log('=======================================');
-console.log('Server running at http://127.0.0.1:3000/');
-console.log('=======================================');
+app.get('/helloworld', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/googlemap.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('PCompass app listening at http://%s:%s', host, port);
+});
