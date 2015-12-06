5faa044d674f6dbd7ac1aeadf04b571391a97952
James Xue <jamesxue100@gmail.com>
2015-12-06 18:27:18 -0500

    export and import POIs working

--- app.js
+++ app.js
@@ -3,6 +3,51 @@ var app = express();
 var path = require("path");
 var fs = require("fs");
 
+// var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/googlemap.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/game',function(req,res){
+  res.sendFile(path.join(__dirname+'/game.html'));
+});
+
+app.get('/builder',function(req,res){
+  res.sendFile(path.join(__dirname+'/builder.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function (socket) {
+  // console.log('hi')
+  // socket.emit('news', { hello: 'world' });
+  socket.on('POIs', function (data) {
+    console.log(data);
+    var writePath = __dirname + '/data.json';
+    fs.writeFile(writePath, JSON.stringify(data), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+	}); 
+
+  });
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('PCompass app listening at http://%s:%s', host, port);
+});
+
+
 
 //var app = require('express').createServer();
 // var io = require('socket.io')(express);
@@ -23,28 +68,28 @@ var fs = require("fs");
 
 
 
-app.get('/helloworld', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/helloworld', function (req, res) {
+//   res.send('Hello World!');
+// });
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/googlemap.html'));
-  //__dirname : It will resolve to your project folder.
-});
+// app.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/googlemap.html'));
+//   //__dirname : It will resolve to your project folder.
+// });
 
-app.get('/game',function(req,res){
-  res.sendFile(path.join(__dirname+'/game.html'));
-});
+// app.get('/game',function(req,res){
+//   res.sendFile(path.join(__dirname+'/game.html'));
+// });
 
-app.get('/builder',function(req,res){
-  res.sendFile(path.join(__dirname+'/builder.html'));
-});
+// app.get('/builder',function(req,res){
+//   res.sendFile(path.join(__dirname+'/builder.html'));
+// });
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-var server = app.listen(process.env.PORT || 3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// var server = app.listen(process.env.PORT || 3000, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
 
-  console.log('PCompass app listening at http://%s:%s', host, port);
-});
+//   console.log('PCompass app listening at http://%s:%s', host, port);
+// });
