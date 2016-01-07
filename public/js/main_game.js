f9e3cdba0a249bd83dbb5ae93a301b6f69b90aba
James Xue <jamesxue100@gmail.com>
2016-01-07 18:20:13 -0500

    center is autocomplete on map, map fits bounds to center

--- public/js/main_game.js
+++ public/js/main_game.js
@@ -63,65 +63,8 @@ function submit() {
       game.push(question)
 }
 
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
 
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  var markers = [];
-  // [START region_getplaces]
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-  // [END region_getplaces]
-}
 
 
 
@@ -176,9 +119,9 @@ function initAutocomplete() {
 
 
     // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
+    var input = document.getElementById('pac-input2');
     var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function() {
@@ -211,7 +154,20 @@ function initAutocomplete() {
     }
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
+     google.maps.event.addDomListener(input, 'keydown', function(e) { 
+      if (e.keyCode == 13) 
+      { 
+              if (e.preventDefault) 
+              { 
+                      e.preventDefault(); 
+              }
+        } 
+      });
+
     searchBox.addListener('places_changed', function() {
+
+
+
       pointsDB = [];
       points = [];
       var places = searchBox.getPlaces();
@@ -279,6 +235,12 @@ function initAutocomplete() {
           //       selectPOI(pointsDB, compass_center); 
           //     }
           // pcompass.drawNeedles();
+          if (place.geometry.viewport) {
+         // Only geocodes have viewport.
+         bounds.union(place.geometry.viewport);
+       } else {
+         bounds.extend(place.geometry.location);
+       }
          });
       
       // points = [];
@@ -286,7 +248,9 @@ function initAutocomplete() {
       // console.log(tablePoints);
       // tablePoints = [];
       // pointsDB = [];
-    // map.fitBounds(bounds);
+       
+
+    map.fitBounds(bounds);
     });
 
       // map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('overlay'));
